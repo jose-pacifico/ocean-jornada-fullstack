02bb6f00f62fea298aee500b292a74e28c27887c
Jose Pacifico <alberto14.filho@gmail.com>
2023-07-18 11:00:58 -0400

    estruturando os cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
 
   return (
     <>
-    <div className='card'>
-      <h1>Título do Item</h1>
-      <img src="https://sm.ign.com/ign_br/cover/s/star-wars-/star-wars-the-mandalorian_6gd6.jpg" alt="imagem do card" />
+    <div className='cards-list'>
+      <Card />
+      <Card />
+      <Card />
     </div>
     </>
   )
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import './Card.css';
+
+export default function Card() {
+    return<>
+        <div className='card'>
+            <h1>Título do Item</h1>
+            <img src="https://sm.ign.com/ign_br/cover/s/star-wars-/star-wars-the-mandalorian_6gd6.jpg" alt="imagem do card" />
+        </div>
+    </>
+}
